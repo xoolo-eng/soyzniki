32000fd3d5000f33653907c6aab2561e8ee51fad
xoolo-dev <development@xoolo-dev.com>
2017-08-03 21:55:34 +0300

    парсинг урла

--- soyzniki/static/map/js/new_map.js
+++ soyzniki/static/map/js/new_map.js
@@ -13,16 +13,16 @@ var storage = {
         points - данные последнего запроса точек по одному сервису;
 
     поле 'name' хранит название ключа для сохранения в локальном
-        хранилище или куках
-        */
-        var support = true;
-        var country = ACTIV_COUNTRY;
-        var map = {name: 'map', coords: false, zoom: 4, scroll: 0};
-        var servis = {name: 'servis', point: false};
-        var filters = {name: 'filters', transport: false, region: false};
-        var points = {name: 'points', data: null};
+    хранилище или куках
+    */
+    var support = test_support();
+    var country = ACTIV_COUNTRY;
+    var map = {name: 'map', coords: false, zoom: 4, scroll: 0};
+    var servis = {name: 'servis', point: false};
+    var filters = {name: 'filters', transport: false, region: false, search: false};
+    var points = {name: 'points', data: false};
 
-        test_support: function() {
+    function() test_support{
         /*
         проверка на поддержку локальноно хранилища, если поддержки
         нет, данные запишутся в куки
@@ -30,27 +30,48 @@ var storage = {
         try {
             window.localStorage;
             suppotr = true;
-        } catch (e) {
+        }
+        catch (e) {
             suppotr false;
         }
-    },
-    update_storage: function(obj) {
+    };
+
+    function update_storage(obj) {
         /*
         обнавление данных в хранилище браузера
         */
         if (support) {
-            /*
-                !!! написать проверку на переполнение
-                локального хранилища
-            */
-            window.localStorage[obj.name] = obj;
+            try {
+                window.localStorage[obj.name] = obj;
+            }
+            catch (e) {
+                if (e.number == 22) {
+                    window.localStorage.clear();
+                    try {
+                        window.localStorage[obj.name] = obj;
+                    }
+                    catch (e) {
+                        if (e.number == 22) {
+                            console.log('Переполнение хранилища')
+                        }
+                    }
+                }
+            }
         }
         else {
             $.cookie(obj.name, JSON.stringify(obj), {
                 path: '/map/'
             });
         }
+    };
+
+    is_clear: function() {
+        if(!filters.transport and !filters.region and !filters.search and !points.data and !servis.point)
+            {return true;}
+        else
+            {return false;}
     },
+
     load_storage: function() {
         /*
         загрузка данных хранилища в память
@@ -82,51 +103,93 @@ var storage = {
             }
         }
     },
-    read_storage: function() {
+    write_storage: function() {
         /*
         запись данных хранилища из памяти
         */
-        this.update_storage(map);
-        this.update_storage(servis);
-        this.update_storage(filters);
-        this.update_storage(points);
+        update_storage(map);
+        update_storage(servis);
+        update_storage(filters);
+        update_storage(points);
     },
-    get_map_data: function() {
+    get_map: function() {
         var map_data = map;
         delete map_data.name;
         return map_data;
     },
-    set_map_data: function(obj) {
+    set_map: function(obj) {
         if(obj.coords !== undefined) map.coords = obj.coords;
         if(obj.zoom !== undefined) map.zoom = obj.zoom;
         if(obj.scroll !== undefined) map.scroll = obj.scroll;
-        this.update_storage(map);
+        update_storage(map);
     },
-    get_servis_data: function() {
+    clear_map: function() {
+        map.coords = false;
+        map.zoom = 4;
+        map.scroll = 0;
+    },
+    get_servis: function() {
         var servis_data = servis;
         delete servis_data.name;
         return servis_data;
     },
-    set_service_data: function(servis_point) {
+    set_servis: function(servis_point) {
         servis.poinst = servis_point;
-        this.update_storage(servis)
+        update_storage(servis)
     },
-    get_filters_data: function() {
+    clear_servis: function() {
+        servis.point = false;
+    }
+    get_filters: function() {
         var filters_data = filters;
         delete filters_data.name;
         return filters_data;
     }
-    set_filters_data: function(obj) {
-
+    set_filters: function(obj) {
+        if(obj.transport !== undefined) filters.transport = obj.transport;
+        if(obj.region !== undefined) filters.region = obj.region;
+        if(obj.search !== undefined) filters.search = obj.search;
+        update_storage(filters);
     },
+    clear_filters: function() {
+        filters.transport = false;
+        filter.region = false;
+        filters.search = false;
+    },
+    get_points: function() {
+        var points_data = points;
+        delete points_data.name;
+        return points_data;
+    },
+    set_points: function() {
+        var points_data = points;
+        delete points;
+        return points_data;
+    }
+    clear_points: function() {
+        points.data = false;
+    }
 };
+
+/*
+*
+обработка url на предмет региона и/или активного сервиса
+*
+*/
+function parse_url() {
+    /*
+    Разбор url на части. Возвращает массив из значений после первого слеша
+    */
+    var url = window.location.pathname;
+    var url_list = url.split('/').slice(1, -2);
+    return url_list;
+}
 
 /*
 *
 Функция инициализации приложения
 *
 */
-
 function initialaze() {
     /*
     Настройка ajax для корректной отправки post запросов
@@ -143,13 +206,23 @@ function initialaze() {
             }
         }
     });
+    storage.load_storage();
+    if(stroage.is_clear()) {
+        /*
+        разбор урл
+        если значений больше двух
+        проверка последних значений
+        на предмет совпадения с регионами
+        и названиями точек
+        если есть совпадения, то заргрузка нужных данных
+        */
+    }
 }
 
-
 $(document).ready(function() {
-
+    initialaze();
 });
 
 $(window).upload(function() {
-
+    storage.write_storage();
 });

b5520a4cfff2f344d9d0421dae2a2266065f2f94
Амельченко Дмитий <admin@soyzniki.ru>
2017-08-03 17:04:09 +0300

    скрытие полей в объекте хранилище

--- soyzniki/static/map/js/new_map.js
+++ soyzniki/static/map/js/new_map.js
@@ -1,3 +1,9 @@
+/*
+*
+Объект хранилище, для сохранения настроек проиложения
+*
+*/
+
 var storage = {
     /*
     Объект хранилище. Держит в памяти значения последних 'действий':
@@ -8,30 +14,35 @@ var storage = {
 
     поле 'name' хранит название ключа для сохранения в локальном
         хранилище или куках
-    */
-    support: true,
-    map: {name: 'map', coords: false, zoom: 4, scroll: 0},
-    servis: {name: 'servis', point: false},
-    filters: {name: 'filters', transport: false, region: false},
-    points: {name: 'points', data: null},
+        */
+        var support = true;
+        var country = ACTIV_COUNTRY;
+        var map = {name: 'map', coords: false, zoom: 4, scroll: 0};
+        var servis = {name: 'servis', point: false};
+        var filters = {name: 'filters', transport: false, region: false};
+        var points = {name: 'points', data: null};
 
-    test_support: function() {
+        test_support: function() {
         /*
         проверка на поддержку локальноно хранилища, если поддержки
         нет, данные запишутся в куки
         */
         try {
             window.localStorage;
-            this.suppotr = true;
+            suppotr = true;
         } catch (e) {
-            this.suppotr false;
+            suppotr false;
         }
     },
     update_storage: function(obj) {
         /*
         обнавление данных в хранилище браузера
         */
-        if (this.support) {
+        if (support) {
+            /*
+                !!! написать проверку на переполнение
+                локального хранилища
+            */
             window.localStorage[obj.name] = obj;
         }
         else {
@@ -44,32 +55,30 @@ var storage = {
         /*
         загрузка данных хранилища в память
         */
-        if (this.support) {
-            if (window.localStorage['map'] !== undefined){
-                this.map = window.localStorage['map'];
+        if (support) {
+            if (window.localStorage[country]['map'] !== undefined){
+                map = window.localStorage[country]['map'];
             }
-            if (window.localStorage['servis'] !== undefined){
-                this.servis = window.localStorage['servis'];
+            if (window.localStorage[country]['servis'] !== undefined){
+                servis = window.localStorage[country]['servis'];
             }
-            if (window.localStorage['filters'] !== undefined){
-                this.filters = window.localStorage['filters'];
+            if (window.localStorage[country]['filters'] !== undefined){
+                filters = window.localStorage[country]['filters'];
             }
-            if (window.localStorage['points'] !== undefined){
-                this.points = window.localStorage['points'];
+            if (window.localStorage[country]['points'] !== undefined){
+                points = window.localStorage[country]['points'];
             }
+            window.localStorage.clear();
         }
         else {
-            if ($.cookie('map') !== undefined) {
-                this.map = JSON.parse($.cookie('map'));
+            if ($.cookie(country + '_map') !== undefined) {
+                map = JSON.parse($.cookie(country + '_map'));
             }
-            if ($.cookie('servis') !== undefined) {
-                this.servis = JSON.parse($.cookie('servis'));
+            if ($.cookie(country + '_servis') !== undefined) {
+                servis = JSON.parse($.cookie(country + '_servis'));
             }
-            if ($.cookie('filters') !== undefined) {
-                this.filters = JSON.parse($.cookie('filters'));
-            }
-            if ($.cookie('points') !== undefined) {
-                this.points = JSON.parse($.cookie('points'));
+            if ($.cookie(country + '_filters') !== undefined) {
+                filters = JSON.parse($.cookie(country + '_filters'));
             }
         }
     },
@@ -77,25 +86,70 @@ var storage = {
         /*
         запись данных хранилища из памяти
         */
-        this.update_storage(this.map);
-        this.update_storage(this.servis);
-        this.update_storage(this.filters);
-        this.update_storage(this.points);
+        this.update_storage(map);
+        this.update_storage(servis);
+        this.update_storage(filters);
+        this.update_storage(points);
     },
     get_map_data: function() {
-        var map_data = this.map;
+        var map_data = map;
         delete map_data.name;
         return map_data;
     },
-    set_map_data: function(map_coords, map_zoom, map_scroll) {
-        this.map.coords = map_coords;
-        this.map.zoom = map_zoom;
-        this.map.scroll = map_scroll;
-        this.update_storage(this.map);
+    set_map_data: function(obj) {
+        if(obj.coords !== undefined) map.coords = obj.coords;
+        if(obj.zoom !== undefined) map.zoom = obj.zoom;
+        if(obj.scroll !== undefined) map.scroll = obj.scroll;
+        this.update_storage(map);
     },
     get_servis_data: function() {
-        var servis_data = this.servis;
+        var servis_data = servis;
         delete servis_data.name;
-        return servis_data
+        return servis_data;
+    },
+    set_service_data: function(servis_point) {
+        servis.poinst = servis_point;
+        this.update_storage(servis)
+    },
+    get_filters_data: function() {
+        var filters_data = filters;
+        delete filters_data.name;
+        return filters_data;
     }
+    set_filters_data: function(obj) {
+
+    },
 };
+
+/*
+*
+Функция инициализации приложения
+*
+*/
+
+function initialaze() {
+    /*
+    Настройка ajax для корректной отправки post запросов
+    с учетом защиты от csrf
+    */
+    var csrftoken = $.cookie('csrftoken');
+    function csrfSafeMethod(method) {
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+}
+
+
+$(document).ready(function() {
+
+});
+
+$(window).upload(function() {
+
+});

49f0cae2f2f6c05fdcaf526dfc5e25a08f6dbac3
Амельченко Дмитий <admin@soyzniki.ru>
2017-08-04 15:45:38 +0300

    поиск через гет запрос

--- map/urls.py
+++ map/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^get_lat_lng$', views.get_lat_lng),
     url(r'^find_point$', views.find_point),
     url(r'^get_info$', views.get_info),
-    url(r'^tiles/(?P<s>[\w]+)/(?P<z>[\w]+)/(?P<x>[\w]+)/(?P<y>[\w]+).png$', views.get_tiles, name='get_tiles'),
-    url(r'^(?P<country_name>[\w]+)/', views.map, name='map'),
+    url(r'^', views.map_load, name='map_load'),
+    # url(r'^tiles/(?P<s>[\w]+)/(?P<z>[\w]+)/(?P<x>[\w]+)/(?P<y>[\w]+).png$', views.get_tiles, name='get_tiles'),
 ]
--- map/views.py
+++ map/views.py
@@ -1,16 +1,19 @@
 from django.template.context_processors import csrf
 from soyzniki.main.auth import is_login, user_id
-from django.http import HttpResponse
-from soyzniki.models import Country
+from django.http import HttpResponse, Http404
+from soyzniki.models import Country, Region
 # from django.core.cache import cache
 from django.shortcuts import render
 from partner.models import Point
 from map.models import Services
+from django.contrib import messages
 from user.models import User
 import json
+from multiprocessing.dummy import Pool as ThreadPool
 
 
 def link_to_name(string):
+    string = string.replace(' ', '_').replace('+', '_')
     list_string = string.split('_')
     if len(list_string) > 1:
         for line in list_string:
@@ -20,9 +23,325 @@ def link_to_name(string):
         return string.capitalize()
 
 
+def find(line):
+    '''
+    проверка на регистронезависимое
+    совпадение по названию сервиса
+    '''
+    try:
+        Services.objects.get(name_ru__iexact=line)
+    except Services.DoesNotExist:
+        try:
+            Services.objects.get(name_en__iexact=line)
+        except Services.DoesNotExist:
+            pass
+        except Services.MultipleObjectsReturned:
+            return {'servis': {
+                'value': line,
+                'accuracy': False,
+                'method': 'exact',
+                'lang': 'en'
+            }}
+        else:
+            return {'servis': {
+                'value': line,
+                'accuracy': True,
+                'method': 'exact',
+                'lang': 'en'
+            }}
+    except Services.MultipleObjectsReturned:
+        return {'servis': {
+            'value': line,
+            'accuracy': False,
+            'method': 'exact',
+            'lang': 'ru'
+        }}
+    else:
+        return {'servis': {
+            'value': line,
+            'accuracy': True,
+            'method': 'exact',
+            'lang': 'ru'
+        }}
+    '''
+    проверка на регистронезависимое
+    вхождение по названию сервиса
+    '''
+    try:
+        Services.objects.get(name_ru__icontains=line)
+    except Services.DoesNotExist:
+        try:
+            Services.objects.get(name_en__icontains=line)
+        except Services.DoesNotExist:
+            pass
+        except Services.MultipleObjectsReturned:
+            return {'servis': {
+                'value': line,
+                'accuracy': False,
+                'method': 'contains',
+                'lang': 'en'
+            }}
+        else:
+            return {'servis': {
+                'value': line,
+                'accuracy': True,
+                'method': 'contains',
+                'lang': 'en'
+            }}
+    except Services.MultipleObjectsReturned:
+        return {'servis': {
+            'value': line,
+            'accuracy': False,
+            'method': 'contains',
+            'lang': 'ru'
+        }}
+    else:
+        return {'servis': {
+            'value': line,
+            'accuracy': True,
+            'method': 'contains',
+            'lang': 'ru'
+        }}
+    '''
+    проверка на регистронезависимое
+    совпадение по названию страны
+    '''
+    try:
+        Country.objects.get(name_ru__iexact=line)
+    except Country.DoesNotExist:
+        try:
+            Country.objects.get(name_en__iexact=line)
+        except Country.DoesNotExist:
+            pass
+        except Country.MultipleObjectsReturned:
+            return {'country': {
+                'value': line,
+                'accuracy': False,
+                'method': 'exact',
+                'lang': 'en'
+            }}
+        else:
+            return {'country': {
+                'value': line,
+                'accuracy': True,
+                'method': 'exact',
+                'lang': 'en'
+            }}
+    except Country.MultipleObjectsReturned:
+        return {'country': {
+            'value': line,
+            'accuracy': False,
+            'method': 'exact',
+            'lang': 'ru'
+        }}
+    else:
+        return {'country': {
+            'value': line,
+            'accuracy': True,
+            'method': 'exact',
+            'lang': 'ru'
+        }}
+    '''
+    проверка на регистронезависимое
+    вхождение по названию страны
+    '''
+    try:
+        Country.objects.get(name_ru__icontains=line)
+    except Country.DoesNotExist:
+        try:
+            Country.objects.get(name_en__icontains=line)
+        except Country.DoesNotExist:
+            pass
+        except Country.MultipleObjectsReturned:
+            return {'country': {
+                'value': line,
+                'accuracy': False,
+                'method': 'contains',
+                'lang': 'en'
+            }}
+        else:
+            return {'country': {
+                'value': line,
+                'accuracy': True,
+                'method': 'contains',
+                'lang': 'en'
+            }}
+    except Country.MultipleObjectsReturned:
+        return {'country': {
+            'value': line,
+            'accuracy': False,
+            'method': 'contains',
+            'lang': 'ru'
+        }}
+    else:
+        return {'country': {
+            'value': line,
+            'accuracy': True,
+            'method': 'contains',
+            'lang': 'ru'
+        }}
+    '''
+    проверка на регистронезависимое
+    совпадение по названию региона
+    '''
+    try:
+        Region.objects.get(name_ru__iexact=line)
+    except Region.DoesNotExist:
+        try:
+            Region.objects.get(name_en__iexact=line)
+        except Region.DoesNotExist:
+            pass
+        except Region.MultipleObjectsReturned:
+            return {'region': {
+                'value': line,
+                'accuracy': False,
+                'method': 'exact',
+                'lang': 'en'
+            }}
+        else:
+            return {'region': {
+                'value': line,
+                'accuracy': True,
+                'method': 'exact',
+                'lang': 'en'
+            }}
+    except Region.MultipleObjectsReturned:
+        return {'region': {
+            'value': line,
+            'accuracy': False,
+            'method': 'exact',
+            'lang': 'ru'
+        }}
+    else:
+        return {'region': {
+            'value': line,
+            'accuracy': True,
+            'method': 'exact',
+            'lang': 'ru'
+        }}
+    '''
+    проверка на регистронезависимое
+    вхождение по названию региона
+    '''
+    try:
+        Region.objects.get(name_ru__icontains=line)
+    except Region.DoesNotExist:
+        try:
+            Region.objects.get(name_en__icontains=line)
+        except Region.DoesNotExist:
+            pass
+        except Region.MultipleObjectsReturned:
+            return {'region': {
+                'value': line,
+                'accuracy': False,
+                'method': 'contains',
+                'lang': 'en'
+            }}
+        else:
+            return {'region': {
+                'value': line,
+                'accuracy': True,
+                'method': 'contains',
+                'lang': 'en'
+            }}
+    except Region.MultipleObjectsReturned:
+        return {'region': {
+            'value': line,
+            'accuracy': False,
+            'method': 'contains',
+            'lang': 'ru'
+        }}
+    else:
+        return {'region': {
+            'value': line,
+            'accuracy': True,
+            'method': 'contains',
+            'lang': 'ru'
+        }}
+    '''
+    если совпадений не найдени, считаем что это поисковый запрос
+    '''
+    return {'search': {
+        'value': line,
+        'accuracy': False,
+        'method': 'contains',
+        'lang': 'ru'
+    }}
+
+
+def map_load(request):
+    data = {
+        'services': Services.objects.filter(active=True)
+    }
+    try:
+        url = request.META['PATH_INFO']
+    except KeyError:
+        raise Http404
+    url_data = url.split('/')[2:-1]
+    if len(url_data) > 5:
+        url_data = url_data[0:4]
+    pool = ThreadPool(len(url_data))
+    res = pool.map(find, url_data)
+    pool.close()
+    pool.join()
+    result = {}
+    for line in res:
+        for key in line:
+            result[key] = line[key]
+    print(result)
+    data_for_response = {}
+    # получение данных страны (если есть в запросе)
+    if result.get('country'):
+        if result['country']['accuracy']:
+            # если точное совпадение с одиночным результатом
+            if result['country']['method'] == 'exact':
+                if result['country']['lang'] == 'ru':
+                    data_for_response['country'] = Country.objects.get(
+                        name_ru__iexact=result['country']['value']
+                    )
+                if result['country']['lang'] == 'en':
+                    data_for_response['country'] = Country.objects.get(
+                        name_en__iexact=result['country']['value']
+                    )
+            # если не точное совпадение с одиночным результатом
+            if result['country']['method'] == 'contains':
+                if result['country']['lang'] == 'ru':
+                    data_for_response['country'] = Country.objects.get(
+                        name_ru__icontains=result['country']['value']
+                    )
+                if result['country']['lang'] == 'en':
+                    data_for_response['country'] = Country.objects.get(
+                        name_en__icontains=result['country']['value']
+                    )
+        else:
+            # множественный результат
+            pass
+
+    data.update(csrf(request))
+    return render(request, 'map.html', data)
+
+'''
 def map(request, country_name):
     services = Services.objects.filter(active=True)
-    country = Country.objects.get(name_en=link_to_name(country_name))
+    try:
+        country = Country.objects.get(
+            name_en=link_to_name(country_name),
+            active=True
+        )
+    except Country.DoesNotExist:
+        try:
+            country = Country.objects.get(
+                name_ru=link_to_name(country_name),
+                active=True
+            )
+        except Country.DoesNotExist:
+            data = {
+                'services': services
+            }
+            messages.info(request, 'Данной страны нет в списке')
+            messages.info(request, 'или она еще не активирована')
+            data.update(csrf(request))
+            return render(request, 'map.html', data)
     regions = country.region_counrty.all()
     data = {
         'services': services,
@@ -35,6 +354,7 @@ def map(request, country_name):
         data['user'] = user
     data.update(csrf(request))
     return render(request, 'map.html', data)
+'''
 
 
 def find_point(request):
@@ -92,14 +412,13 @@ def get_lat_lng(request):
     if request.is_ajax() and request.method == 'POST':
         country = request.POST.get('country')
         result = Country.objects.get(name_en=country)
-        print(result)
         data = {
             'lat_lng': [result.latitude, result.longitude],
         }
         return HttpResponse(json.dumps(data))
 
 
-def get_tiles(request, s, z, x, y):
-    from urllib.request import urlopen
-    url = 'http://{0}.tile.osm.org/{1}/{2}/{3}.png'
-    return HttpResponse(urlopen(url.format(s, int(z), int(x), int(y))))
+# def get_tiles(request, s, z, x, y):
+#     from urllib.request import urlopen
+#     url = 'http://{0}.tile.osm.org/{1}/{2}/{3}.png'
+#     return HttpResponse(urlopen(url.format(s, int(z), int(x), int(y))))
